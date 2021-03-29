08407df6dd2771305ad3ab100f08a8ebf5837366
vanthao03596 <vanthao03596@users.noreply.github.com>
2021-03-29 10:41:04 +0000

    Fix styling

--- src/HasPasswordHistory.php
+++ src/HasPasswordHistory.php
@@ -11,13 +11,13 @@ trait HasPasswordHistory
     public static function bootHasPasswordHistory()
     {
         static::saved(function (Model $model) {
-            if (!$password = Arr::get($model->getChanges(), 'password')) {
+            if (! $password = Arr::get($model->getChanges(), 'password')) {
                 return;
             }
 
             $model->passwordHistories()->create([
                 'changed_at' => now(),
-                'password' => $password
+                'password' => $password,
             ]);
         });
     }
