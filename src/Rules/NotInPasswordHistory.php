9655cf9b2ca7a1a7d7e46fad12f000683c92ab63
phamthao <phamthao03596@gmail.com>
2021-03-30 15:16:04 +0700

    fix: psalm show error

--- src/Rules/NotInPasswordHistory.php
+++ src/Rules/NotInPasswordHistory.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Hash;
 
 class NotInPasswordHistory implements Rule
 {
+
     protected $user;
 
     protected $month_to_check;
@@ -14,10 +15,10 @@ class NotInPasswordHistory implements Rule
     public function __construct($user)
     {
         $this->user = $user;
-        $this->month_to_check = config('password-history.months_to_check');
+        $this->month_to_check = config('password-history.months_to_check', 12);
     }
 
-    public function passes($attribute, $value)
+    public function passes($attribute, $value): bool
     {
         $passwordHistories = $this->user->passwordHistories()
             ->where('changed_at', '>=', now()->subMonths($this->month_to_check))
@@ -32,6 +33,11 @@ class NotInPasswordHistory implements Rule
         return true;
     }
 
+    /**
+     * @psalm-suppress InvalidReturnType
+     * @psalm-suppress NullableReturnStatement
+     * @psalm-suppress InvalidNullableReturnType
+     */
     public function message()
     {
         return __('password-history.used_in_pass_month', ['month' => $this->month_to_check]);
