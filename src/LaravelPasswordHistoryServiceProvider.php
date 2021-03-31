e0ea5bc93cdae7da2081925c6d60f813cab2f2c6
phamthao <phamthao03596@gmail.com>
2021-03-31 22:54:37 +0700

    chore: vanthao03596/laravel-package-tools

--- src/LaravelPasswordHistoryServiceProvider.php
+++ src/LaravelPasswordHistoryServiceProvider.php
@@ -2,17 +2,14 @@
 
 namespace Vanthao03596\LaravelPasswordHistory;
 
+use Vanthao03596\LaravelPackageTools\Package;
+use Vanthao03596\LaravelPackageTools\PackageServiceProvider;
 use Vanthao03596\LaravelPasswordHistory\Commands\LaravelPasswordHistoryCommand;
 
 class LaravelPasswordHistoryServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-password-history')
             ->hasConfigFile()
